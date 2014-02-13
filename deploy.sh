e888a29b00ce14f5d838fd01dfe51ffd305ad24e
Dana Woodman <dana@danawoodman.com>
2014-02-13 13:07:19 -0800

    update deploy script messages

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+echo -e "\033[0;32mBuilding project...\033[0m"
 
 # Build the project.
 hugo -s ./
+
+echo -e "\033[0;32mCommitting changes...\033[0m"
 
 # Add changes to git.
 git add .
@@ -14,6 +16,8 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
 # Push source and build repos.
 git push origin master
